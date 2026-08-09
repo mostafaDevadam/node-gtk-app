f0094997e0c36b0478274dfd78081507475be380
mostafaDevadam <adam.mostafa@outlook.de>
2026-08-09 12:03:47 +0300

    comment code

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,11 @@ import Adw from 'gi:Adw-1'
 import Gdk from 'gi:Gdk-4.0'
 import GObject from 'gi:GObject-2.0'
 
+/*
+const gi = require('node-gtk');
+const Gtk = gi.require('Gtk', '4.0');
+const Gdk = gi.require('Gdk', '4.0');
+const Gio = gi.require('Gio', '2.0');
+*/
 
 export {GLib, Gio, Gtk, Adw, Gdk, GObject }
